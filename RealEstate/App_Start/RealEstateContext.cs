87699d6fdf7dd325629f631a2186063141bd015b
Zoltán Tóth <zshun86@gmail.com>
2014-08-08 17:31:08 +0200

    Initial app created.

---
+++ RealEstate/App_Start/RealEstateContext.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using RealEstate.Properties;
+
+namespace RealEstate
+{
+    public class RealEstateContext
+    {
+        private readonly MongoDatabase _database;
+        public RealEstateContext()
+        {
+            var client = new MongoClient(Settings.Default.RealEstateConnectionString);
+            var server = client.GetServer();
+            _database = server.GetDatabase(Settings.Default.RealEstateDatabase);
+        }
+
+        public MongoDatabase Database {
+            get { return _database; }
+        }
+    }
+}
---
+++ RealEstate/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstate.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly RealEstateContext _dbContext = new RealEstateContext();
+
+        public ActionResult Index()
+        {
+            _dbContext.Database.GetStats();
+            return Json(_dbContext.Database.Server.BuildInfo, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
