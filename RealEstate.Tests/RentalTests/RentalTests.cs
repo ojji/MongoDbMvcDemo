5cef78a350eeacbc66330f4a9d182c4d33b91df4
Zoltán Tóth <zshun86@gmail.com>
2014-08-09 16:35:05 +0200

    The basic Rental model type created.

---
+++ RealEstate.Tests/RentalTests/RentalTests.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using NUnit.Framework;
+using RealEstate.Rentals;
+
+namespace RealEstate.Tests.RentalTests
+{
+    [TestFixture]
+    public class RentalTests : AssertionHelper
+    {
+        [Test]
+        public void RentalModel_Price_Is_Represented_As_Double_In_BsonDocument()
+        {
+            var rental = new Rental() {Price = 15m};
+            var rentalDocument = rental.ToBsonDocument();
+
+            Expect(rentalDocument["Price"].IsDouble, True);
+        }
+
+        [Test]
+        public void RentalModel_Id_Is_Represented_As_An_ObjectId()
+        {
+            var rental = new Rental();
+            rental.Id = ObjectId.GenerateNewId().ToString();
+            var rentalDocument = rental.ToBsonDocument();
+
+            Expect(rentalDocument["_id"].BsonType, Is.EqualTo(BsonType.ObjectId));
+        }
+      
+    }
+}
---
+++ RealEstate/Rentals/Rental.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RealEstate.Rentals
+{
+    public class Rental
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Description { get; set; }
+        public int NumberOfRooms { get; set; }
+        public List<string> Address = new List<string>();
+
+        [BsonRepresentation(BsonType.Double)]
+        public decimal Price { get; set; }
+    }
+}
