5e83fe7644562f996cab557aa4223c6b82bd3c24
Zoltán Tóth <zshun86@gmail.com>
2014-08-10 14:20:10 +0200

    Basic GridFS file creation, replacing. Added 'Back to rentals' link to price adjusting view.

--- RealEstate/Controllers/RentalsController.cs
+++ RealEstate/Controllers/RentalsController.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using MongoDB.Bson;
-using MongoDB.Driver;
 using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
 using MongoDB.Driver.Linq;
 using RealEstate.Rentals;
 using RealEstate.ViewModels;
@@ -84,5 +84,66 @@ namespace RealEstate.Controllers
             return _context.Rentals.FindOneById(new ObjectId(id));
         }
 
+        public ActionResult AttachImage(string id)
+        {
+            var rental = GetRental(id);
+            return View(rental);
+        }
+
+        [HttpPost]
+        public ActionResult AttachImage(string id, HttpPostedFileBase file)
+        {
+            var rental = GetRental(id);
+            if (file != null)
+            {
+                if (rental.HasImage())
+                {
+                    DeleteImage(rental);
+                }
+
+                StoreImage(file, rental);
+                return RedirectToAction("Index");
+            }
+
+            return View(rental);
+        }
+
+        private void DeleteImage(Rental rental)
+        {
+            _context.Database.GridFS.DeleteById(new ObjectId(rental.ImageId));
+            SetRentalImageId(rental.Id, null);
+        }
+
+        private void StoreImage(HttpPostedFileBase file, Rental rental)
+        {
+            var imageId = ObjectId.GenerateNewId();
+            SetRentalImageId(rental.Id, imageId.ToString());
+
+            var options = new MongoGridFSCreateOptions
+            {
+                Id = imageId,
+                ContentType = file.ContentType
+            };
+
+            _context.Database.GridFS.Upload(file.InputStream, file.FileName, options);
+        }
+
+        private void SetRentalImageId(string id, string imageId)
+        {
+            var rentalById = Query<Rental>.Where(r => r.Id == id);
+            var setRentalImageId = Update<Rental>.Set(r => r.ImageId, imageId);
+            _context.Rentals.Update(rentalById, setRentalImageId);
+        }
+
+        public ActionResult GetImage(string id)
+        {
+            var image = _context.Database.GridFS.FindOneById(new ObjectId(id));
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+
+            return File(image.OpenRead(), image.ContentType);
+        }
     }
 }
--- RealEstate/Rentals/Rental.cs
+++ RealEstate/Rentals/Rental.cs
@@ -16,6 +16,10 @@ namespace RealEstate.Rentals
         public List<string> Address = new List<string>();
         public List<PriceAdjustment> PriceAdjustments = new List<PriceAdjustment>();
 
+        [BsonRepresentation(BsonType.Double)]
+        public decimal Price { get; set; }
+        public string ImageId { get; set; }
+
         public Rental()
         {
             
@@ -29,14 +33,17 @@ namespace RealEstate.Rentals
             Address = (postRental.Address ?? string.Empty).Split('\n').ToList();
         }
 
-        [BsonRepresentation(BsonType.Double)]
-        public decimal Price { get; set; }
 
         public void AdjustPrice(AdjustPrice adjustPrice)
         {
             var adjustment = new PriceAdjustment(Price, adjustPrice);
             PriceAdjustments.Add(adjustment);
             Price = adjustPrice.NewPrice;
+        }
+
+        public bool HasImage()
+        {
+            return !string.IsNullOrWhiteSpace(ImageId);
         }
     }
 }
